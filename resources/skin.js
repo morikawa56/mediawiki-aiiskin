21f026daaf1e64729292752263df472b189c7d03
morikawa56 <morikawa@kimisui56.work>
2024-03-16 16:06:31 +0800

    修改加载方式

--- resources/skin.js
+++ resources/skin.js
@@ -3,14 +3,9 @@ $( function () {
 	/* scripts can go here */
 	const Vue = require( 'vue' );
 	const App = require( './App.vue' );
-	const appEl = document.getElementById( 'app' );
-	console.log( 'appEl:', appEl );
 	// Only enhance to Vue if browser support is good enough
 	if ( typeof fetch !== 'undefined' && typeof Object.assign !== 'undefined' ) {
 		// eslint-disable-next-line no-new
-		new Vue( {
-			el: appEl,
-			render: h => h(App)
-		} );
+		Vue.createMwApp(App).mount('#app')
 	}
 } );
