3f0bb53a0dd98d044ba0904e874680ced4d4f2b6
morikawa56 <morikawa@kimisui56.work>
2024-03-16 11:34:38 +0800

    vue 测试

--- resources/skin.js
+++ resources/skin.js
@@ -6,7 +6,7 @@ $(
         const App = require( './App.vue' );
         const appEl = document.getElementById( 'app' );
         Vue.use(ElementUI);
-
+        console.log(111);
         new Vue({
             el: appEl,
             render: h => h(App)
