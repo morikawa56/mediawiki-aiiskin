2bf2c1188159ca06105abef57bade429d47711ad
morikawa56 <morikawa@kimisui56.work>
2024-03-16 14:28:51 +0800

    test

--- resources/skin.js
+++ resources/skin.js
@@ -1,23 +1,27 @@
 /* JavaScript for the AiiSkin skin */
-$(
-    function () {
-        const ElementUI = require( '../utils/element-ui/index.js' );
-        const App = require( './App.vue' );
-        const appEl = document.getElementById( 'app' );
-        Vue.use(ElementUI);
-        console.log('helloworld');
-        if ( typeof fetch !== 'undefined' && typeof Object.assign !== 'undefined' ) {
-            // eslint-disable-next-line no-new
-            new Vue( {
-                el: appEl,
-                render: function ( createElement ) {
-                    return createElement( App, {
-                        props: {
-                            initialData: JSON.parse( appEl.dataset.json )
-                        }
-                    } );
-                }
-            } );
-        }
-    }
-);
+$( function () {
+	/* scripts can go here */
+	const Vue = require( 'vue' );
+	const App = require( './App.vue' );
+	const appEl = document.getElementById( 'app' );
+    console.log( 'appEl: ', appEl );
+	// Only enhance to Vue if browser support is good enough
+	if ( typeof fetch !== 'undefined' && typeof Object.assign !== 'undefined' ) {
+		// eslint-disable-next-line no-new
+		new Vue( {
+			el: appEl,
+			/**
+			 *
+			 * @param {Function} createElement
+			 * @return {Vue.VNode}
+			 */
+			render: function ( createElement ) {
+				return createElement( App, {
+					props: {
+						initialData: JSON.parse( appEl.dataset.json )
+					}
+				} );
+			}
+		} );
+	}
+} );
